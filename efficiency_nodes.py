a86c2f5fcb359d720469260e30d95164c5ee2fb0
DorotaL <122320001+DorotaLuna@users.noreply.github.com>
2023-06-14 03:01:10 +0000

    select all display mode in "preview_image"

--- efficiency_nodes.py
+++ efficiency_nodes.py
@@ -516,8 +516,7 @@ class TSC_KSampler:
                      "negative": ("CONDITIONING",),
                      "latent_image": ("LATENT",),
                      "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
-                     "preview_image": (["Disabled", "Enabled"],),
-                     "preview_grid": (["Disabled", "Enabled"],),
+                     "preview_image": (["Disabled", "Enabled", "Grid"],),
                      },
                 "optional": { "optional_vae": ("VAE",),
                               "script": ("SCRIPT",),},
@@ -531,7 +530,7 @@ class TSC_KSampler:
     CATEGORY = "Efficiency Nodes/Sampling"
     
     def sample(self, sampler_state, model, seed, steps, cfg, sampler_name, scheduler, positive, negative,
-               latent_image, preview_image, preview_grid, denoise=1.0, prompt=None, extra_pnginfo=None, my_unique_id=None,
+               latent_image, preview_image, denoise=1.0, prompt=None, extra_pnginfo=None, my_unique_id=None,
                optional_vae=(None,), script=None):
 
         # Extract node_settings from json
@@ -1499,7 +1498,7 @@ class TSC_KSampler:
             update_value_by_id("results", my_unique_id, results)
 
             # Squeeze and Stack the tensors, and store results
-            if preview_grid == "Disabled":
+            if preview_image != "Grid":
                 image_tensor_list = torch.stack([tensor.squeeze() for tensor in image_tensor_list])
             else:
                 image_tensor_list = images
